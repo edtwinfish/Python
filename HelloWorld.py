046dfcbb1d041e4b765a329dfa07359268875c7b
evelyn <edtwinfish@gmail.com>
2018-04-23 18:58:28 -0700

    initial python playtime notes

---
+++ HelloWorld.py
@@ -0,0 +1,34 @@
+print('Hello World and other Python playtime notes')
+print('\n\nstrings can use single or double quotes \n'
+      'inside quotes can be escaped with backslash')
+print('\n\nstring on multiple lines with single print statement must\n'
+      'have each line begin and end with quotes\n'
+      ', and the entire \nstring must begin and end with the parenthesis')
+print('\n\nto print long string including a new line use \\n \'backslash n\'')
+print('\n\nto cascade multiple if statements\n'
+      'use \'if: elif: else\:\'')
+
+x = 1;
+y = 2;
+z = 3;
+
+if x > y:
+    print ('if is true')
+
+elif x < y:
+    print ('elif is true')
+
+else:
+    print ('fell to else')
+
+print('\n\nfunctions use indented line to contain the function'
+      '\ndef precedes the function name to define it.'
+      '\nto call the function just type the function name followed by open '
+      'and closed parenthesis')
+
+def example():
+    print('inside example function')
+
+print('\ncalling example function now')
+example()
+
